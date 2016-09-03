6914793508f102552da304301fc9db884ed2865e
kpam92 <kpam1992@gmail.com>
2016-09-03 14:26:17 -0700

    added new user to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(username: 'user1', password: 'password')
+User.create!(username: 'SallyMander', password: 'password')
 
 Photo.create!(title:"kevin",medium:"digital", description:"man and dog",
              author_id:1, album_id:1, cover_photo: false,
@@ -16,8 +17,8 @@ Photo.create!(title:"ACA",medium:"digital", description:"2016 team",
              author_id:1, album_id:1, cover_photo: false,
              image_url:"http://res.cloudinary.com/dt5viyxyq/image/upload/v1472767620/a1_g7gv2m.jpg")
 Photo.create!(title:"Girl & Hands",medium:"ink",
-             author_id:1, album_id:1, cover_photo: false,
+             author_id:2, album_id:2, cover_photo: false,
              image_url:"http://res.cloudinary.com/dt5viyxyq/image/upload/v1472768974/girl_and_hands_y20osh.jpg")
 Photo.create!(title:"Vanessa with flowers",medium:"film",
-             author_id:1, album_id:1, cover_photo: false,
+             author_id:2, album_id:2, cover_photo: false,
              image_url:"http://res.cloudinary.com/dt5viyxyq/image/upload/v1472768971/stephen_9010-2_rti9cb.jpg")
