9807e6d46c0aee23dc5a1583a3eb2c56c8a400ec
kpam92 <kpam1992@gmail.com>
2016-09-06 13:23:52 -0700

    old migration file was faulty

--- db/migrate/20160901014558_create_photos.rb
+++ db/migrate/20160901014558_create_photos.rb
@@ -1,5 +1,15 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
+    create_table :photos do |t|
+      t.string :title, null: false
+      t.string :medium
+      t.text :description
+      t.integer :author_id, null: false
+      t.integer :album_id, null: false
+      t.boolean :cover_photo, default: false
+      t.string :image_url, null: false
+      t.timestamps
+    end
     add_index :photos, :author_id
     add_index :photos, :album_id
   end
