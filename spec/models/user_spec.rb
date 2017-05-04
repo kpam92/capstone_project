c51e5b980a97207b3ec6e939ce44b14f1a8db207
kpam92 <kpam1992@gmail.com>
2017-05-03 17:35:46 -0700

    factory girl config added in application.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:username) }
+  let!(:user) { User.create!(username:'guest111',password:'password')}
+
+  describe 'validations' do
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:password_digest)}
+    it {should validate_presence_of(:session_token)}
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_length_of(:username).is_at_least(6)}
+
+  end
+
 end
